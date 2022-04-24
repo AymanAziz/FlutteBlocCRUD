c2200c4d3bb72422dce7ca723a60d20113eea1c0
AymanAziz <89571593+AymanAziz@users.noreply.github.com>
2022-04-24 21:08:08 +0800

    Bloc is completed

---
+++ lib/business_layer/controller/Delete/delete_april_state.dart
@@ -0,0 +1,10 @@
+part of 'delete_april_bloc.dart';
+
+abstract class DeleteAprilState extends Equatable {
+  const DeleteAprilState();
+}
+
+class DeleteAprilInitial extends DeleteAprilState {
+  @override
+  List<Object> get props => [];
+}
